6179369deca03fd620dc4f657e05b57525496b25
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-08 15:51:04 +0300

    edit height for scaled screen

--- haftalik_odevler/hafta_2/todo-app/src/components/TodoList.js
+++ haftalik_odevler/hafta_2/todo-app/src/components/TodoList.js
@@ -4,7 +4,7 @@ const TodoList = ({ todos, toggleCompleted, deleteTodo }) => {
     return (
         //Todoları listelemek için todos arrayini mapliyoruz ve itemleri listeliyoruz
         //Mapping the todos array to list the items in the TodoList
-        <div className="todo-list h-40 sm:h-64 overflow-y-auto pr-5 pt-1 mt-4">
+        <div className="todo-list h-40 sticky sm:h-58 overflow-y-auto pr-5 pt-1 mt-4">
             {todos.map((todo) => (
                 <TodoItem
                     key={todo.id}
