7e375cf3018ecd7455980f99008162b2b8ac5ec9
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-15 00:41:31 +0300

    Get 7 day weather data

--- haftalik_odevler/hafta_3/weather-app/src/contexts/WeatherContext.js
+++ haftalik_odevler/hafta_3/weather-app/src/contexts/WeatherContext.js
@@ -7,20 +7,25 @@ const WeatherContext = createContext();
 
 //Burada bir provider oluşturuyorum ve weatherData ve setWeatherData statelerini tutuyorum
 //Create a provider because i will keep states weatherData , setWeatherDate 
-export function WeatherProvider({ children }) {
+export function WeatherProvider({ children, selectedCity, units, lang}) {
 
     //Hava durumu bilgisinin tutulması ve değiştirilmesi için kullanılacak stateler
     const [weatherData, setWeatherData] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const apiKey = "1689a5fee00c14519d8e643f2a55fe6d";
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&lang=tr&appid=${apiKey}`);
-            const data = response.data;
-            setWeatherData(data);
-        }
-        fetchData();
-    },[]);
+        const apiKey = "1689a5fee00c14519d8e643f2a55fe6d";
+        //OpenWeathermap api ı kullanarak 5 günlük/3 saatlik hava tahminini çekiyorum
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity},tr&units=${units}&lang=${lang}&appid=${apiKey}`;
+
+        axios.get(url)
+            .then(response => {
+                const data = response.data.list;
+                setWeatherData(data);
+            })
+            .catch(error => {
+                console.log("Hava durumu verileri çekilirken bir hata oluştu. / An error occurred while retrieving weather data.", error)
+            });
+    }, [selectedCity, units, lang]);
 
     console.log(weatherData);
 
--- haftalik_odevler/hafta_3/weather-app/src/pages/weather.js
+++ haftalik_odevler/hafta_3/weather-app/src/pages/weather.js
@@ -4,16 +4,20 @@ import WeatherContext from '../contexts/WeatherContext';
 export default function Weather() {
     const { weatherData } = useContext(WeatherContext);
 
-    console.log("burada" , weatherData);
+    console.log("burada", weatherData);
     return (
         <div>
             {weatherData ? (
                 <div>
-                    <h1>Name: {weatherData.name}</h1>
-                    <p>Açıklama: {weatherData.weather[0].description}</p>
-                    <p>Sıcaklık: {parseInt(weatherData.main.temp)}</p>
-                    <p>Nem: {weatherData.main.humidity}</p>
-                    <p>Rüzgar hızı: {weatherData.wind.speed}</p>
+                    {weatherData.map((day, index) => (
+                        <div key={index}>
+                            <h3>{new Date(day.dt * 1000).toLocaleDateString()}</h3>
+                            <p>Açıklama: {day.weather[0].description}</p>
+                            <p>Sıcaklık: {parseInt(day.main.temp)}</p>
+                            <p>Nem: {day.main.humidity}</p>
+                            <p>Rüzgar hızı: {day.wind.speed}</p>
+                        </div>
+                    ))}
                 </div>
             ) : (
                 <p>Loading...</p>
