8fcb657a223cc97baadcea17582f219ecd13b6af
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-20 03:13:22 +0300

    create: nested route structure

--- haftalik_odevler/hafta_4/modern_book_app/src/components/books.js
+++ haftalik_odevler/hafta_4/modern_book_app/src/components/books.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { getBooks } from './data'
 
-export default function books() {
+export default function Books() {
+    let books = getBooks();
+
     return (
-        <div>
-            <div>Books page</div>
-            <Outlet></Outlet>
-        </div>
+        <main className='text-center'>
+            <div>Books</div>
+            <div className='flex flex-col gap-3'>
+                {books.map(book => (
+                    <Link
+                        className='bg-slate-600'
+                        to={`books/${book.number}`}
+                        key={book.number}
+                    >
+                        {book.name}
+                    </Link>
+                ))}
+            </div>
+        </main>
     )
 }
---
+++ haftalik_odevler/hafta_4/modern_book_app/src/components/data.js
@@ -0,0 +1,21 @@
+let books = [
+    {
+        name:"Mustafa",
+        number:"1",
+        description:"Mustafa açıklama"
+    },
+    {
+        name:"Şahin",
+        number:"2",
+        description:"Şahin açıklama"
+    },
+    {
+        name:"FMSS",
+        number:"3",
+        description:"FMSS açıklama"
+    }
+]
+
+export function getBooks() {
+    return books;
+}
