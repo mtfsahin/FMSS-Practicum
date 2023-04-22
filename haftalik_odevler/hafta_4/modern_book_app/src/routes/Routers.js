364f5f3bc1097a510bce64a1d604ef890e4d544f
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-04-22 23:04:26 +0300

    update: css update

--- haftalik_odevler/hafta_4/modern_book_app/src/routes/Routers.js
+++ haftalik_odevler/hafta_4/modern_book_app/src/routes/Routers.js
@@ -1,6 +1,6 @@
-import About from '../components/book';
-import Books from '../components/book';
+import About from '../components/about';
 import Details from '../components/details';
+import Home from '../components/home';
 
 import { Routes, Route, } from 'react-router-dom'
 
@@ -8,18 +8,17 @@ const Routers = () => {
     return (
         <Routes>
             {/* I write other routes in / so that the routes do not disappear from the screen when the pages change */}
-            <Route path="/">
+            <Route path="/" element={<Home />} />          
                 {/* after listing the books, when you click on the book, it creates a route according to the id of the book */}
-                <Route path="/books" element={<Books />}></Route>
-                <Route path="/books/:bookId" element={<Details />} ></Route>
+                <Route path="/books/:bookId" element={<Details />} />
                 {/* if searched page is not found this will be this output */}
                 <Route path="*" element={
                     <main>
                         <div className='text-center bg-cyan-500 p-4'>404 Page</div>
                     </main>
-                }></Route>
-                <Route path="/about" element={<About />}></Route>
-            </Route>
+                } />
+                <Route path="/about" element={<About />} />
+
         </Routes>
     );
 };
